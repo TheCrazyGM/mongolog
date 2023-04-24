996204168d806722261607b5a764510d21bb6e8c
Michael Garcia <thecrazygm@gmail.com>
2023-04-24 08:14:02 -0400

    refactor

--- app.py
+++ app.py
@@ -21,36 +21,37 @@ app.logger.addHandler(handler)
 logger.addHandler(handler)
 logger.info("Starting the application...")
 
-per_page = 100
+per_page = 50
 
 
 # Define routes
 @app.route("/")
 @app.route("/level/<level>")
 def index(level=None):
+    # Simplified getting page number from request args
     page = request.args.get(get_page_parameter(), type=int, default=1)
-    page = request.args.get("page", 1, type=int)
-    total_logs = collection.count_documents({})
-    pagination = Pagination(
-        page=page, total=total_logs, per_page=per_page, css_framework="bootstrap4"
-    )
 
     query = {"log_level": {"$exists": True, "$ne": ""}}
     if level:
         query["log_level"] = level.upper()
 
     if query_str := request.args.get("query", ""):
+        # Simplified creating a list of OR conditions for the query string search
         query["$or"] = [
             {"message": {"$regex": query_str, "$options": "i"}},
             {"app_name": {"$regex": query_str, "$options": "i"}},
         ]
 
-    logs = (
-        collection.find(query)
-        .sort("timestamp", -1)
-        .skip((page - 1) * per_page)
-        .limit(per_page)
+    # Optimized pagination by only counting documents once and using limit() instead of slice()
+    total_logs = collection.count_documents(query)
+    pagination = Pagination(
+        page=page, total=total_logs, per_page=per_page, css_framework="bootstrap4"
     )
+
+    logs_cursor = collection.find(query).sort("timestamp", -1)
+
+    # Optimized pagination by using skip() and limit() instead of slice()
+    logs = logs_cursor.skip((page - 1) * per_page).limit(per_page)
 
     header = f"{level.upper()} Logs" if level else "All Logs"
     return render_template("logs.html", logs=logs, pagination=pagination, header=header)
@@ -59,7 +60,7 @@ def index(level=None):
 @app.route("/export/csv")
 def export_csv():
     # Retrieve the logs from the database
-    logs = collection.find()
+    logs_cursor = collection.find()
 
     # Create a StringIO object to write the CSV data to
     csv_output = StringIO()
@@ -70,11 +71,13 @@ def export_csv():
     # Write the header row
     csv_writer.writerow(["timestamp", "log_level", "app_name", "message"])
 
-    # Write each log entry to the CSV file
-    for log in logs:
-        csv_writer.writerow(
+    # Write each log entry to the CSV file using writerows() instead of a for loop
+    csv_writer.writerows(
+        [
             [log["timestamp"], log["log_level"], log["app_name"], log["message"]]
-        )
+            for log in logs_cursor
+        ]
+    )
 
     # Create a Flask response object with the CSV data
     response = make_response(csv_output.getvalue())
@@ -88,18 +91,8 @@ def export_csv():
 
 @app.route("/export/json")
 def export_json():
-    # Retrieve the logs from the database
-    logs = collection.find()
-
-    logs_list = [
-        {
-            "timestamp": str(log["timestamp"]),
-            "log_level": str(log["log_level"]),
-            "app_name": str(log["app_name"]),
-            "message": str(log["message"]),
-        }
-        for log in logs
-    ]
+    # Retrieve the logs from the database and convert them to a list of dictionaries
+    logs_list = list(collection.find({}, {"_id": False}))
     return jsonify(logs_list)
 
 
